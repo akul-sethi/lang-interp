986e44037885493c4df0d334abba2b257ca6a2c9
Akul Sethi <sethi.ak@northeastern.edu>
2024-11-14 18:24:50 -0500

    not test, calculates mean activations

--- main.py
+++ main.py
@@ -3,27 +3,37 @@ from nnsight import CONFIG, LanguageModel
 import os
 from dotenv import load_dotenv
 import torch
+import numpy as np
+from transformers import AutoTokenizer
 
 load_dotenv()
 CONFIG.set_default_api_key(os.environ["API_KEY"])
 
+llm = LanguageModel("meta-llama/Meta-Llama-3.1-8B")
+ds = load_dataset("bigcode/humanevalpack", lang, trust_remote_code=True)["test"]
+LANGS = ["python", "js", "java", "go", "cpp", "rust"]
 
-def ma_across_prompt_type(dataset):
+
+def ma_across_prompt_type(lang):
     """Assume data set takes from rows=(prompt type) cols=(language). returns dataset: rows=(layer) cols=(language)"""
-    llm = LanguageModel("bigcode/starcoder2-15b")
-    LAYERS = 40
-    DIMS = 6144
-    PROMPTS, LANGS = dataset.shape
+    LAYERS = 32
+    DIMS = 4096
+    PROMPTS = len(ds)
 
     # [prompt, layer, lang, dims]
-    layers = torch.tensor([PROMPTS, LAYERS, LANGS, DIMS])
+    layers = torch.tensor([LAYERS, DIMS])
+    states = [[None for _ in range(LAYERS)] for _ in range(PROMPTS)]
     for prompt in range(PROMPTS):
-        for lang in range(LANGS):
-            with llm.trace(dataset[prompt][lang]) as tracer:
-                for layer in range(LAYERS):
-                    layers[prompt, layer, lang, :] = llm.model.layers[
-                        layer
-                    ].output.save()
+        with llm.trace(ds[prompt]["prompt"]) as tracer:
+            for layer in range(LAYERS):
+                states[prompt][layer] = llm.model.layers[layer].output.save()
+
+    for prompt in range(PROMPTS):
+        for layer in range(LAYERS):
+            layers[layer, :] += states[prompt][layer]
+
+    return layers / PROMPTS
 
 
-ma_across_prompt_type(None)
+for lang in LANGS:
+    torch.save(ma_across_prompt_type(lang), f"{lang}.pt")
