41aa61ebd0472e4587c6856c6c86fb4a87180529
Akul Sethi <sethi.ak@northeastern.edu>
2024-11-14 18:27:46 -0500

    fixed issue

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ load_dotenv()
 CONFIG.set_default_api_key(os.environ["API_KEY"])
 
 llm = LanguageModel("meta-llama/Meta-Llama-3.1-8B")
-ds = load_dataset("bigcode/humanevalpack", lang, trust_remote_code=True)["test"]
 LANGS = ["python", "js", "java", "go", "cpp", "rust"]
 
 
 def ma_across_prompt_type(lang):
     """Assume data set takes from rows=(prompt type) cols=(language). returns dataset: rows=(layer) cols=(language)"""
+    ds = load_dataset("bigcode/humanevalpack", lang, trust_remote_code=True)["test"]
+
     LAYERS = 32
     DIMS = 4096
     PROMPTS = len(ds)
@@ -24,7 +25,7 @@ def ma_across_prompt_type(lang):
     layers = torch.tensor([LAYERS, DIMS])
     states = [[None for _ in range(LAYERS)] for _ in range(PROMPTS)]
     for prompt in range(PROMPTS):
-        with llm.trace(ds[prompt]["prompt"]) as tracer:
+        with llm.trace(ds[prompt]["prompt"], remote=True) as tracer:
             for layer in range(LAYERS):
                 states[prompt][layer] = llm.model.layers[layer].output.save()
 
